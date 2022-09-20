01fb22bc44fe45bf5e5c35c23c9fbaee2f31ee4d
Kate Raf <KateSu1989@gmail.com>
2022-09-20 08:12:34 +0500

    Exercise 004

---
+++ Exercise004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа по определению четных чисел внутри введенного числа
+
+Console.WriteLine("Введите целое число");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= N)
+{
+if ((i % 2) == 0)
+Console.WriteLine(i);
+i++;
+}
