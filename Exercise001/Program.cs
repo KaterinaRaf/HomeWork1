c79975f83dfcfbe2ecf8997eaebf62a45da499d0
Kate Raf <KateSu1989@gmail.com>
2022-09-20 08:03:17 +0500

    Exercise 001

---
+++ Exercise001/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа по выявлению максимально числа из 2-х введенных
+
+Console.WriteLine("Введите первое целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Наибольшее из введенных чисел {a}");
+}
+else
+{
+    Console.WriteLine($"Наибольшее из введенных чисел {b}");
+}
