1e3fd9eb44b0ae070f99ac36f0991b7297243c06
Kate Raf <KateSu1989@gmail.com>
2022-09-20 08:06:03 +0500

    Exercise 002

---
+++ Exercise002/Program.cs
@@ -0,0 +1,27 @@
+﻿// Программа по выявлению максимально числа из 3-х введенных
+
+Console.WriteLine("Введите первое целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье целое число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int Max = a;
+
+if (Max < b)
+{
+    Max = b;
+}
+else if (Max < c)
+{
+    Max = c;
+}
+else
+{
+    Max = a;
+}
+
+Console.WriteLine($"Наибольшее из введенных чисел {Max}");
