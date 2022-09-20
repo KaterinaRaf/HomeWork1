46f77641c5d1553a461ea1672b398ea2dde6f70f
Kate Raf <KateSu1989@gmail.com>
2022-09-20 08:09:21 +0500

    Exercise 003

---
+++ Exercise003/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа по определению четности числа
+
+Console.WriteLine("Введите целое число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if ((x % 2) == 0)
+{
+    Console.WriteLine("Введенное число четное!");
+}
+else
+{
+    Console.WriteLine("Введенное число НЕ четное!");
+}
