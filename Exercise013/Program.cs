7e4f5b0d85b485e845f79128c3487602c0d286c9
Kate Raf <KateSu1989@gmail.com>
2022-09-21 21:46:28 +0500

    Exercise 013

---
+++ Exercise013/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет
+
+int CreateNumber()  //генерация случайного числа
+{
+    return new Random().Next(0,1000);
+}
+
+int ShowThirdDigit(int Number) //описание метода
+{
+    if (Number < 100)
+    {
+        return -1;
+    }
+        return Number % 10;
+}
+
+int Number = CreateNumber();
+int Third = ShowThirdDigit(Number);
+
+Console.WriteLine(Number);
+
+if (Third ==-1)
+{
+    Console.WriteLine("Третьей цифры в числе нет! Увы!");
+}
+else
+{
+    Console.WriteLine("Третья цифра в числе " +Third);
+}
