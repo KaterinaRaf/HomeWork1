5f8550343a9e48474c75488a53b0ccba05efccd0
Kate Raf <KateSu1989@gmail.com>
2022-09-21 21:24:00 +0500

    Exercise 010

---
+++ Exercise010/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа
+
+int CreateNumber()
+{
+    return new Random().Next(100,1000);
+}
+int ShowSecondDigit(int Number)
+{
+    int Hundred = Number / 100;
+    int Ed = Number % 10;
+    int Dec = (Number - Hundred*100 - Ed) / 10;
+    return Dec;
+}
+
+int Number = CreateNumber();
+Console.WriteLine(Number);
+Console.WriteLine(ShowSecondDigit(Number));
