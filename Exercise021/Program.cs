a1c5df1a703a0b538562a0b46ec61d0d599c9976
KaterinaRaf <113040268+KaterinaRaf@users.noreply.github.com>
2022-09-27 14:26:46 +0500

    Exercise 021

--- Exercise021/Program.cs
+++ Exercise021/Program.cs
@@ -3,3 +3,18 @@
 //A (3,6,8); B (2,1,-7), -> 15.84
 //A (7,-5, 0); B (1,-1,9) -> 11.53
 
+double GetNumber(string name) // метод для ввода чисел
+{
+    Console.WriteLine("Введите координату " + name);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double x1 = GetNumber("x1");
+double x2 = GetNumber("x2");
+double y1 = GetNumber("y1");
+double y2 = GetNumber("y2");
+double z1 = GetNumber("z1");
+double z2 = GetNumber("z2");
+
+double S = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2) + Math.Pow((z2 - z1),2));
+Console.WriteLine("Расстояние = " + S);
