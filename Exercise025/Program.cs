46788576d7036d3d8c0c545c504efe07c5ee9dd8
KaterinaRaf <113040268+KaterinaRaf@users.noreply.github.com>
2022-09-29 13:41:57 +0500

    Exercise 025

--- Exercise025/Program.cs
+++ Exercise025/Program.cs
@@ -2,3 +2,18 @@
 //и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵).   2, 4 -> 16
 
+int GetNumber(string name) 
+{
+    Console.WriteLine("Введите число " + name);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int A = GetNumber("вводимое в степень");
+int B = GetNumber("степень");
+
+void GetProduct(int A, int B)
+{
+  Console.WriteLine("Число " + A + " в степени " + B + " равно " + Math.Pow(A,B));
+}
+
+GetProduct(A, B);
