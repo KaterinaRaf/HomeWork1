f86358e3c457d500dfe4926c6265b294e8471717
Kate Raf <KateSu1989@gmail.com>
2022-10-02 13:28:34 +0500

    Exercise 027

--- Exercise027/Program.cs
+++ Exercise027/Program.cs
@@ -25,52 +25,20 @@ int GetDigitFromNumber(int Number, int index) // Получение значен
     int num = Number/Convert.ToInt32(Math.Pow(10,LengthNum(Number)-index))%10;
     return num;
 }
- 
-int GetSum(number)
+
+ int GetSum(int Number)   // получение суммы цифр в числе
 {
-  int num = GetNumber(number)
-    
-    for (int i = 1; i <= num; i++)
-      
-      
-Console.WriteLine("Вторая цифра: " + GetDigitFromNumber(num,2));
-Console.WriteLine("Третья цифра: " + GetDigitFromNumber(num,3));
-Console.WriteLine("Четвертая цифра: " + GetDigitFromNumber(num,4));
-Console.WriteLine("Пятая цифра: " + GetDigitFromNumber(num,5));
-
-
-
-
-
-
-
-//
-int GetNumber()
-{
-            Console.WriteLine("Введите число: ");
-      return Convert.ToInt32(Console.ReadLine());
-}
-
-int GetSum (int num)
-{
-    int i = 0;
-    int sum = 0;
-     
-     while(num < 0 || num > 0)
+  int sum = 0;
+  int Len = LengthNum(Number);
+  
+     for (int i = 0; i <= Len; i++)
     {
-        num = Math.Abs(num / 10);  // модуль числа
-        sum = 
-       
-       i++;
-    }
-      
-      
-      for (int i = 1; i <= number; i++)
-    {
-        sum = sum + i;
+        int Digit = GetDigitFromNumber(Number, i);
+        sum += Digit;
     }
     return sum;
-}
-
+}      
+      
 int number = GetNumber();
+Console.WriteLine("Длина введеного числа = " + LengthNum(number));
 Console.WriteLine("Сумма цифр в веденном числе равна: " + GetSum(number));
