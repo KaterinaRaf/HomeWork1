943ec03e6ac0c23f1406e96d14a38795bea4ab6c
Kate Raf <KateSu1989@gmail.com>
2022-10-02 13:58:50 +0500

    Exercise 029

--- Exercise029/Program.cs
+++ Exercise029/Program.cs
@@ -3,4 +3,39 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+void FillArrayUser(int[] arr)  //заполнение массива пользователем
+{
+    Console.WriteLine("Введите число для массива, согласно введеному объему массива: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
 
+void FillArray(int[] arr)  //заполнение массива рандомом
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-1000,1000);
+    }
+}
+
+void PrintArray(int[] arr)   //вывод на экран массива
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+Console.WriteLine("Введите количество элеметов (объем) массива: ");
+int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+ //int[] array = new int[8];   // задавание массива с опред. длинной [8]
+
+ FillArrayUser(array);
+ PrintArray(array);
+ Console.WriteLine("Массив введенный пользователем");
+
+ FillArray(array);
+ PrintArray(array);
+ Console.WriteLine("Массив из рандомных чисел");
