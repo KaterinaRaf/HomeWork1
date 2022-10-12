be4bf9e7e1db836e1630fee0391c8c8fa4d5f2d1
Kate Raf <KateSu1989@gmail.com>
2022-10-12 23:12:25 +0500

    Exercuse 038

---
+++ Exercise038/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+//Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+void FillArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-100, 100);
+    }
+}
+
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+double GetMaxNumber(double[] arr)
+{
+    double max = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max;
+}
+
+double GetMinNumber(double[] arr)
+{
+    double min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < min)
+            min = arr[i];
+    }
+    return min;
+}
+
+double[] array = new double[6];
+FillArray(array);
+PrintArray(array);
+double maxNumber = GetMaxNumber(array),
+       minNumber = GetMinNumber(array),
+       difference = maxNumber - minNumber;
+Console.WriteLine($"Разница между максимальным {maxNumber} и минимальным {minNumber} элементами равна {difference}");
+Console.WriteLine();
+
