2b9ff28b9a399a3fe43a7b9135b6a6247d65e10f
Kate Raf <KateSu1989@gmail.com>
2022-10-16 20:18:39 +0500

    Exercise 066

---
+++ Exercise066/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+double GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double SumFor(double lowNumber, double hightNumber)
+{
+    double result = 0;
+    if (lowNumber < hightNumber)
+    {
+        result = lowNumber + SumFor(lowNumber + 1, hightNumber);
+    }
+    else if (lowNumber == hightNumber)
+    {
+         result = result + hightNumber;
+    }
+    return result;
+}
+
+//КОД ОСНОВНОЙ ПРОГРАММЫ
+double m = Math.Truncate(GetNumberFromConsole("Введите нижнюю границу промежутка")),
+       n = Math.Truncate(GetNumberFromConsole("Введите верхнюю границу промежутка"));
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n} = {SumFor(m, n)}");
